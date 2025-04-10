48a942260fc43d2dfb0546c97fecce3e49798b9e
AdithyanMS175 <adithyanms175@gmail.com>
2025-04-10 20:17:24 +0530

    dd

---
+++ src/Components/Navbar.jsx
@@ -0,0 +1,17 @@
+import React from 'react'
+import styles from './Navbar.module.css'
+const Navbar = () => {
+  return (
+    <div>
+        <nav className={styles.nav-wrapper}>
+            <div className={styles.nav-content}>
+                <img className={styles.logo} src="" alt="" />
+
+            </div>
+        </nav>
+
+    </div>
+  )
+}
+
+export default Navbar
